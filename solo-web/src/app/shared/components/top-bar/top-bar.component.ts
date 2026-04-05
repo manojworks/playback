dd90bd00b58c55dc8c283ba8f95292920f80b1ad
Manoj Saxena <42760785+manojworks@users.noreply.github.com>
2026-04-05 15:28:17 -0400

    improved ui and a placeholder icon

--- solo-web/src/app/shared/components/top-bar/top-bar.component.ts
+++ solo-web/src/app/shared/components/top-bar/top-bar.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss']
 })
